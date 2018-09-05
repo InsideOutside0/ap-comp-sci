d16bf76e4f33d1a3fa3fe5a7477c7943cd0a0727
InsideOutside0 <sammy@salwen.org>
2018-09-05 09:31:30 -0700

    Adds Exponent and Fibonacci functions

---
+++ src/FibonacciExp.java
@@ -0,0 +1,47 @@
+/*
+Sam Stahl
+AP Computer Science
+Fibonacci and Exponents
+*/
+
+import java.util.Scanner;
+
+public class FibonacciExp {
+
+    public static int fibonacci(int term) {
+        // The function is mathematically recursive
+        // It would be a shame if I didn't use recursion
+        if (term<0) return -1;
+        else if (term==0) return 0;
+        else if (term==1) return 1;
+        return fibonacci(term-1)+fibonacci(term-2);
+    }
+
+    public static double exp(int base, int power) {
+        if (base==0) return 0; //the function cannot return undefined
+        else if (power==0) return 1;
+        double total = base;
+        boolean isNeg = false;
+        if (power<0) {
+            isNeg = true;
+            power*=-1;
+        }
+        for (int i=1; i<power; i++) {
+            total*=base;
+        }
+        if (isNeg) total=1/total;
+        return total;
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a term number in the fibonacci sequence");
+        int term = sc.nextInt();
+        System.out.println("Enter a number as the base of an exponent");
+        int base = sc.nextInt();
+        System.out.println("Enter a number as the exponent");
+        int power = sc.nextInt();
+        System.out.println("\n"+fibonacci(term));
+        System.out.println(exp(base, power));
+    }
+}
