aefa3def34b2f8401dbaf662924ca3d1f1b907ae
InsideOutside0 <sammy@salwen.org>
2018-09-13 09:49:22 -0700

    Adds singularize

--- src/ChangeMaker.java
+++ src/ChangeMaker.java
@@ -6,6 +6,14 @@ Change Giver/Maker
 
 public class ChangeMaker {
 
+    public static String singularize(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int length = sb.length();
+        sb = sb.deleteCharAt(length-3);
+        s = sb.toString();
+        return s;
+    }
+
     public static String makeChange(double price, double payment) {
         if (payment<price) return "Pay the full price, please.";
         else if (payment==price) return "Exact change! Thank you very much!";
@@ -15,13 +23,15 @@ public class ChangeMaker {
             int difference = centPayment - centPrice;
             int[] values = {10000, 5000, 2000, 1000, 500, 100, 50, 25, 10, 5, 1};
             String[] types = {" $100 bills, ", " $50 bills, ", " $20 bills, ", " $10 bills, ", " $5 bills, ", " $1 bills, ",
-                                " half dollars, ", " quarters, ", " dimes, ", " nickels, ", " pennies."};
+                                " half dollars, ", " quarters, ", " dimes, ", " nickels, ", " one cent coins. "};
             String result = "Your change is: ";
             for (int i=0; i<values.length; i++) {
                 if (difference>=values[i]) {
                     int change = difference/values[i];
                     difference-=change*values[i];
-                    result+=change+types[i];
+                    String input = types[i];
+                    if (change==1 ) input = singularize(input);
+                    result+=change+input;
                 }
             }
             return result;
