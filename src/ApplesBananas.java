ce56b42af64dc78e8e3b2acffabc0552b5098f80
InsideOutside0 <sammy@salwen.org>
2018-09-27 09:54:46 -0700

    Begins Apples and Bananas

---
+++ src/ApplesBananas.java
@@ -0,0 +1,11 @@
+public class ApplesBananas {
+
+    public static String vowelizeApple(int i) {
+        String[] parts = {"ap", "ay-", "ee-", "i-", "oh-", "oo-", };
+        return parts[i];
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
